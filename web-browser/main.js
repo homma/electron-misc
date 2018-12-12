05a694263b7acdaa929ded2b8c78aa423989f717
homma <daisuke.homma@gmail.com>
2018-12-12 22:59:18 +0900

    committed on 2018/12/12 22:59.

--- web-browser/main.js
+++ web-browser/main.js
@@ -1,29 +1,59 @@
 const config = require("./config");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserView, BrowserWindow } = require("electron");
 
 const url = config.start_url;
 
-const createWindow = (width, height) => {
+const createWindow = () => {
   const properties = {
-    width: width,
-    height: height,
+    x: 0,
+    y: 0,
+    width: config.window.width,
+    height: config.window.height
+  };
 
+  const win = new BrowserWindow(properties);
+
+  // const tb = createToolbar(win);
+  const br1 = createBrowser(win);
+
+  return win;
+};
+
+const createToolbar = win => {
+  const properties = {
+    x: 0,
+    y: 0,
+    height: 100,
+    width: config.window.width
+  };
+
+  const view = new BrowserView(properties);
+
+  win.setBrowserView(view);
+  view.setBounds({x:0, y:0, width: 1000, height: 100});
+  return view;
+};
+
+const createBrowser = win => {
+  const properties = {
     webPreferences: {
       nodeIntegration: false,
       allowRunningInsecureContent: true
     }
   };
 
-  const win = new BrowserWindow(properties);
+  const view = new BrowserView(properties);
 
-  win.loadURL(url);
+  // cannot have multipel browser view
+  win.setBrowserView(view);
+  view.setBounds({x:0, y:0, width: 1000, height: 800});
+  view.webContents.loadURL(url);
+
+  return view;
 };
 
 const main = () => {
-  const width = config.window.width;
-  const height = config.window.height;
-
-  createWindow(width, height);
+  createWindow();
 };
 
 app.on("ready", main);
